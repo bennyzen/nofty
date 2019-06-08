78f287597d845f763f64bd92c5d7cb41039eddb3
bennyzen <benny.zaminga@gmail.com>
2019-06-08 16:00:56 +0200

    working example

---
+++ config.js
@@ -0,0 +1,9 @@
+// define global constants herein
+const config = {
+  WALL_THICKNESS: 2.5,
+  SEGMENT_OUTER_DIAMETER: 80,
+  SEGMENT_HEIGHT: 40,
+  POT_INNER_DIAMETER: 34,
+  POT_HEIGHT: 30,
+}
+module.exports = config
