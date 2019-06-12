e101e206de3e3c6b151dd21c824564f0295cb10f
bennyzen <benny.zaminga@gmail.com>
2019-06-12 23:01:15 +0200

    adds the updated and finetuned config

--- config.js
+++ config.js
@@ -1,9 +1,21 @@
-// define global constants herein
+/*
+Use this configuration file to finetune your salatower. Every printer is
+different, so you'll probably need to adjust these values to make things fit
+into each other. Values set herein are global and affect all parts of the
+system. Tweak them carefully and one by one.
+*/
 const config = {
-  WALL_THICKNESS: 2.5,
+  WALL_THICKNESS: 2,
   SEGMENT_OUTER_DIAMETER: 80,
-  SEGMENT_HEIGHT: 40,
+  SEGMENT_HEIGHT: 50 + 12,
   POT_INNER_DIAMETER: 34,
-  POT_HEIGHT: 30,
+  POT_HEIGHT: 35,
+  RING_HEIGHT: 12,
+  RING_INSET: 4.5,  // play with this value, to make your middle segments fit into each other
+  TOP_DIAMETER: 75.5, // play with this value, to make your top segment fit
+  END_HEIGHT: 40,
+  FLOW_DIAMETER: 12,
+  FLOW_HEIGHT: 10,
+  ROPE_DIAMETER: 8,
 }
 module.exports = config
